03369eb3e387b0f65242a12904b97cde816bab83
Andrew Beauchemin <android@vt.edu>
2022-07-01 11:36:01 -0400

    filled out gremlinservice

--- src/main/java/com/revature/service/GremlinService.java
+++ src/main/java/com/revature/service/GremlinService.java
@@ -1,4 +1,15 @@
 package com.revature.service;
 
+import com.revature.model.Gremlin;
+import com.revature.repository.GremlinRepository;
+
+import java.util.List;
+
 public class GremlinService {
+    private GremlinRepository gremlinRepository = new GremlinRepository();
+
+    public  List<Gremlin> getAllGremlins(){return gremlinRepository.getAll();}
+
+    public Gremlin createGremlin(Gremlin gremlin){return gremlinRepository.create(gremlin);};
+
 }
