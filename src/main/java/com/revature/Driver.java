1f9359733ed87cb0c3cc8cef609429b1218d72a7
Andrew Beauchemin <android@vt.edu>
2022-07-01 11:46:45 -0400

    finished driver and gremlin controller

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -1,11 +1,14 @@
 package com.revature;
 
+import com.revature.controller.GremlinController;
 import io.javalin.Javalin;
 
 public class Driver {
     public static void main(String[] args) {
+        GremlinController gremlinController = new GremlinController();
         Javalin app = Javalin.create().start(8080);
 
-
+        app.get("/gremlins", gremlinController.getAllGremlins);
+        app.post("/gremlins", gremlinController.createGremlin);
     }
 }
--- src/main/java/com/revature/controller/GremlinController.java
+++ src/main/java/com/revature/controller/GremlinController.java
@@ -15,13 +15,18 @@ public class GremlinController {
         if(gremlins != null){
             ctx.status(200).json(gremlins);
         }else {
-            ctx.status(400);
+            ctx.status(400).result("Error getting Gremlins");
         }
     };
 
     public Handler createGremlin = ctx -> {
         Gremlin gremlin = ctx.bodyAsClass(Gremlin.class);
 
+        if(gremlinService.createGremlin(gremlin)) {
+            ctx.status(200).result("Success creating Gremlin");
+        }else {
+            ctx.status(400).result("Failure creating Gremlin");
+        }
     };
 
 }
